334ca263cfbae48c4072d96605767b0cc64d24ca
hoon <nhoon95@gmail.com>
2020-07-15 06:51:14 +0900

    built webpack completely

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,48 @@ const path = require("path");
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT = path.join(__dirname, "static");
+const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 const config = {
-  entry: ENTRY,
+  entry: ["@babel/polyfill", ENTRY],
   mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ overrideBrowserslist: "cover 99.5%" })];
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ]),
+      },
+    ],
+  },
   output: {
     path: OUTPUT,
-    filename: "main.js",
+    filename: "[name].js",
   },
+  plugins: [new ExtractCSS("styles.css")],
 };
 
 module.exports = config;
